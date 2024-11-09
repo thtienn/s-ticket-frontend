c4bce52f1554921c2c137af289a1b908c0e34d9b
thao-vonguyen <thao.vonguyen@hcmut.edu.vn>
2024-11-09 14:44:57 +0700

    for-login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 // src/App.jsx
 import UserList from './components/UserList';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <UserList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<UserList />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
+import { useNavigate } from 'react-router-dom'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
@@ -8,7 +9,8 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Login() {
-    const [session, setSession] = useState(null)
+    const [session, setSession] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         supabase.auth.getSession().then(({ data: { session } }) => {
@@ -18,18 +20,25 @@ export default function Login() {
         const {
             data: { subscription },
         } = supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session)
+            setSession(session);
+            if (session) {
+                navigate('/#'); // Forward to homepage if logged in
+            }
         })
 
         return () => subscription.unsubscribe()
     }, [])
 
     if (!session) {
-        return (<Auth 
-            supabaseClient={supabase} 
-            appearance={{ theme: ThemeSupa }} 
-            providers={['google']}
-        />)
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <Auth
+                    supabaseClient={supabase}
+                    appearance={{ theme: ThemeSupa }}
+                    providers={['google']}
+                />
+            </div>
+        );
     }
     else {
         return (<div>Logged in!</div>)
